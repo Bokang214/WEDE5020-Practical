504b5ae85ff433c04bd397c3795c44a527729df3
Bokang214 <lenkoanebokang214@gmail.com>
2025-09-18 20:52:37 +0000

    JavaScript
    JavaScript

---
+++ myScript.js
@@ -0,0 +1,23 @@
+    //below is my global varibale
+     var x = 1;
+     console.log(x); //value must be 1
+     if(true){
+        var y = 2;
+     console.log(x); //value must be 1
+}
+
+//prompts and alerts
+var firstName = prompt("Enter your first name here");
+var lastName = prompt("Enter your last name");``
+var age  = prompt("Enter your age");
+var isNumber = true;
+if(age !== isNumber){
+    alert("Please enter number only my dude");
+
+}
+var qualification = prompt("Enter your qualification");
+var institution = prompt("Enter your institution");
+ alert("My name is " + firstName +" " + lastName + " . I am " + age + " years old. I am doing " + qualification + " at " + institution + ".");
+ alert("Thank you");
+
+ 
