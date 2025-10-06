8a6035330ca1ce4e98726f264c88d6e34803bf19
Bokang214 <lenkoanebokang214@gmail.com>
2025-10-06 13:00:00 +0000

    validation
    validation

---
+++ JavaScript Validation/valiScript.js
@@ -0,0 +1,31 @@
+ //Requesting user their first name
+    var user = prompt("Hi, what is your first name?")
+    document.getElementsById("userFirstNmae").innerHTML = user;
+   
+function formValidation(){
+
+    //Requesting user to provide details in 'Contact us'
+    let firstName = document.forms["myForm"]["fname"].value;
+     let lastname = document.forms["myForm"]["lname"].value;
+      let emailAdress = document.forms["myForm"]["email"].value;
+       let emailSubject = document.forms["myForm"]["esub"].value;
+       //var message = document.forms["myForm"]["emessage"].value;
+
+       //Validating textboxes
+       if(firstName === "" ){
+        alert("You required to fill in all the spaces!")
+            return false;
+       }
+       else if(lastname === ""){
+      alert("You required to fill in all the spaces!")
+            return false;
+       }
+       else if(emailAdress === "" ){
+        alert("You required to fill in all the spaces!")
+            return false;
+       }
+       else if(firstName === "" ){
+        alert("You required to fill in all the spaces!")
+            return false;
+       }
+}
